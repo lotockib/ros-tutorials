b8fc1ccd3832ab2e1cce960bf8ac17ab3b0bbb0e
brad <brad@ubuntu-vm.myguest.virtualbox.org>
2023-11-01 17:10:34 -0600

    setup fix

--- setup.sh
+++ setup.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install
+sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt get update
+sudo apt update
 
-sudo apt install ros-kinetic-full -y
+sudo apt install ros-kinetic-desktop-full -y
 sudo apt install python3-catkin-pkg python3-rospkg -y
-sudo apt install python3-rosdep
-pip install numpy pyyaml pyside2 pyqtgraph matplotlib
+sudo apt install python3-rosdep -y
+pip install numpy pyyaml pyside2 pyqtgraph matplotlib -y
 
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
 
 # workaround for issue on vm machine
-sudo apt install --reinstall libxcb-xinerama0
+sudo apt install --reinstall libxcb-xinerama0 -y
+
+# echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+dir=$(pwd)
+cp $dir/.bashrc ~/.bashrc
+# bashrc="/home/brad/.bashrc"
+# while IFS= read line
+# do
+#     echo "$line"
+#     if [[ $line == *"source /opt/ros/"* ]]; 
+#         then echo "IT IS SOURCING"
+#     fi
+# done <"$bashrc"
 
 # setup ros
-sudo rosdep init --reinstall --include-eol-distros
-# todo remove file?  argument
+sudo rm -f /etc/ros/rosdep/sources.list.d/20-default.list
+sudo rosdep init --reinstall
+rosdep update --include-eol-distros
