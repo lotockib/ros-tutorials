01204ed844bec7a6756ab990b97fb02bbadbd111
brad <brad@ubuntu-vm.myguest.virtualbox.org>
2023-11-01 20:01:01 -0600

    setup fixes

--- REAME.md
+++ REAME.md
@@ -1,3 +1,19 @@
 # Scope
 
 Repository to experiment with ros tutorials.
+
+# Setup
+
+1. Verify you are running Ubuntu 16.04
+2. Verify python is version 3.7 with command `python --version`
+3. If python is not version 3.7, download and install Miniconda3-py37_23.1.0-1-Linux-x86_64.sh from [here](https://repo.anaconda.com/miniconda/)
+4. Clone repo
+    ```bash
+    cd ~
+    git clone http://github.com/lotockib/ros-tutorials.git
+    ```
+5. Run setup script
+    ```bash
+    cd ~/ros-tutorials
+    ./setup.sh
+    ```
--- setup.sh
+++ setup.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
+sudo apt install curl -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 
-sudo apt install ros-kinetic-desktop-full -y
-sudo apt install python3-catkin-pkg python3-rospkg -y
-sudo apt install python3-rosdep -y
-pip install numpy pyyaml pyside2 pyqtgraph matplotlib -y
-
+sudo apt install python-catkin-pkg python-rospkg -y
 sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+
+pip install numpy pyyaml pyside2 pyqtgraph matplotlib pydot
 
 # workaround for issue on vm machine
 sudo apt install --reinstall libxcb-xinerama0 -y
 
-# echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+sudo apt install ros-kinetic-desktop-full -y
+
 dir=$(pwd)
 cp $dir/.bashrc ~/.bashrc
 # bashrc="/home/brad/.bashrc"
 # while IFS= read line
 # do
 #     echo "$line"
-#     if [[ $line == *"source /opt/ros/"* ]]; 
+#     if [[ $line == *"source /opt/ros/"* ]];
 #         then echo "IT IS SOURCING"
 #     fi
 # done <"$bashrc"
